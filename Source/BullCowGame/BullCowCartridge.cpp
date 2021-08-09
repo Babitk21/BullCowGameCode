ccb090aab2bf5c22bfd690d9b4a08cb5fce6c45d
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2021-08-09 12:12:43 +0530

    3rd push

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,13 +3,49 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    SetupGame();
+
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to Bull-Cows Game"));
-    PrintLine(TEXT("Press enter to continue...."));
+    PrintLine(FString::Printf(TEXT("the hidden word is %s"),*hiddenWord));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-        FString hiddenWord = TEXT("jean");
+    if(bGameOver==false)
+    {
+        ClearScreen();
+        SetupGame();
+    }
+    else
+    {    
+        if(Input==hiddenWord)
+        {
+           PrintLine(TEXT("you won!"));
+           EndGame();
+        }
+        else
+        {
+            if(Input.Len() != hiddenWord.Len())
+            {
+                PrintLine(FString::Printf(TEXT("the hidden word is %i characters,\n you have lost!"),hiddenWord.Len()));
+                EndGame();
+            }
+        }
+    }   
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    hiddenWord = TEXT("jean");
+    Lives = 3;
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to Bull-Cows Game"));
+    PrintLine(FString::Printf(TEXT("guess the %i letter word"),hiddenWord.Len()));
+    PrintLine(TEXT("Press enter to continue...."));
+}
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to play again!"));
 }
